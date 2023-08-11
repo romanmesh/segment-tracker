d1b335223e9690843089fab4e451dc378edc07da
cdROma <38502733+gthrm@users.noreply.github.com>
2023-08-11 22:30:23 +0200

    init

---
+++ index.js
@@ -0,0 +1,82 @@
+(function (global) {
+  /**
+   * Initializes the Segment tracking, listening to click events to track specified
+   * data and also tracks the page viewed event.
+   */
+  function initSegmentTracking() {
+    document.body.addEventListener("click", handleElementClick);
+    window.addEventListener("popstate", trackPageViewed);
+    window.addEventListener("hashchange", trackPageViewed);
+    trackPageViewed();
+  }
+
+  /**
+   * Event handler for DOM elements clicked. If the clicked element has a `data-track-event`
+   * attribute, it gathers the relevant data and sends it to Segment.
+   *
+   * @param {Event} event - The DOM click event.
+   */
+  function handleElementClick(event) {
+    const eventName = event.target.getAttribute("data-track-event");
+    if (eventName) {
+      const eventData = {
+        ...getDataAttributes(event.target),
+      };
+
+      // Check for analytics availability before calling it
+      if (global.analytics && typeof global.analytics.track === "function") {
+        global.analytics.track(eventName, eventData);
+      } else {
+        console.warn("Segment analytics is not available.");
+      }
+    }
+  }
+
+  /**
+   * Collects all data attributes from a given DOM element that start with `data-event-`.
+   *
+   * @param {HTMLElement} element - The DOM element to gather data attributes from.
+   * @returns {Object} - An object containing the data attributes.
+   */
+  function getDataAttributes(element) {
+    const data = {};
+    for (let attr of element.attributes) {
+      if (attr.name.startsWith("data-event-")) {
+        const key = attr.name.slice("data-event-".length);
+        data[key] = attr.value;
+      }
+    }
+    return data;
+  }
+
+  /**
+   * Gathers page related data like path, referrer, search query, title, and full URL.
+   *
+   * @returns {Object} - An object containing the page data.
+   */
+  function gatherEventData() {
+    return {
+      path: window.location.pathname,
+      referrer: document.referrer,
+      search: window.location.search,
+      title: document.title,
+      url: window.location.href,
+    };
+  }
+
+  /**
+   * Sends a 'Page Viewed' event to Segment with relevant page data.
+   */
+  function trackPageViewed() {
+    const eventData = gatherEventData();
+
+    // Check for analytics availability before calling it
+    if (global.analytics && typeof global.analytics.track === "function") {
+      analytics.track("Page Viewed New", eventData);
+    } else {
+      console.warn("Segment analytics is not available for Page Viewed event.");
+    }
+  }
+
+  initSegmentTracking();
+})(typeof window !== "undefined" ? window : this);
---
+++ readme.md
@@ -0,0 +1,44 @@
+Segment Event Tracker
+=====================
+
+Description
+-----------
+
+This script is designed for automatically tracking events on your website and sending them to Segment. It monitors click events on elements with a `data-track-event` attribute and also automatically sends a "Page Viewed" event upon URL changes or page loads.
+
+Installation
+------------
+
+1. Include the Segment script on your page.
+2. Add this tracking script to your webpage.
+
+Usage
+-----
+
+Attach the `data-track-event` attribute to any DOM element you wish to track. The value of this attribute will be used as the event name in Segment.
+
+For example:
+
+```sh
+<button data-track-event="Button Clicked">Click Me</button>
+```
+
+If you want to pass additional data with the event, you can add attributes that start with `data-event-`.
+
+For instance:
+
+```sh
+<button data-track-event="Button Clicked" data-event-color="red">Click Me</button>
+```
+
+In this example, when the user clicks on the button, a "Button Clicked" event with an additional `color` field having the value "red" will be sent.
+
+Error Handling
+--------------
+
+If the `analytics` object from Segment is not available, the script will log a warning to the console. Ensure the Segment script is loaded and accessible on your page prior to this script.
+
+License
+-------
+
+Please specify licensing information here, if applicable.
